1ee45e140c9ce97fd930424a95b16d662b78cd39
Himanshu09072005 <himanshu090705@gmail.com>
2026-02-11 21:31:32 +0530

    Day 1: First Python program with input and output

---
+++ day1.py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+next_year = age + 1
+
+print("\nHello", name)
+print("You are", age, "years old.")
+print("Next year you will be", next_year, "years old.")
+print("Welcome to Python learning!")
