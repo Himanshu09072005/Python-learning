499c8be5fe231a393cd0d71eea5a95e1fd2f450a
Himanshu09072005 <himanshu090705@gmail.com>
2026-02-12 19:37:51 +0530

    Day 2: Built simple calculator with conditions

---
+++ day2_calculator.py
@@ -0,0 +1,35 @@
+print("Simple Calculator")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+
+choice = input("Enter your choice (1-4): ")
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+if choice == "1":
+    result = num1 + num2
+    print(f"Result: {result}")
+
+
+elif choice == "2":
+    result = num1 - num2
+    print(f"Result: {result}")
+
+
+elif choice == "3":
+    result = num1 * num2
+    print(f"Result: {result}")
+
+
+elif choice == "4":
+    if num2 != 0:
+        result = num1 / num2
+        print(f"Result: {result}")
+    else:
+        print("Error: Division by zero is not allowed")
+
+else:
+    print("Invalid choice")
