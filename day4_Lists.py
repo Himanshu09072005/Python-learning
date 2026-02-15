a4f080cd14e5c0733fa2566aca9f0cfd08a309c1
Himanshu09072005 <himanshu090705@gmail.com>
2026-02-15 11:04:55 +0530

    Day 4: Implemented list handling with Validation and  user input processing

--- day4_Lists.py
+++ day4_Lists.py
@@ -1,10 +1,20 @@
 names = []
 
-for i in range(3):
-    name = input("Enter a name: ").strip().title()
-    names.append(name)
+while True:
+    name = input("Enter a name (or type 'done' to finish): ").strip()
+    
+    # Check if user wants to stop
+    if name.lower() == "done":
+        break
+    
+    # Validate input
+    if name.replace(" ", "").isalpha():
+        clean_name = " ".join(name.title().split())
+        names.append(clean_name)
+    else:
+        print("Invalid input, accepts only letters and spaces.")
 
-print("\n Processed names:")
+print("\nProcessed names:")
 
 for name in names:
     print(f"Welcome {name}")
